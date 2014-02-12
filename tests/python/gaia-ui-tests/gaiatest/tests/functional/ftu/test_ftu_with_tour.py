c63d578da07f8be2854618cffcecd691d7ee67fa
Viorela Ioia <viorelaioia@gmail.com>
2014-02-12 16:59:20 +0200

    Bug 968912 - Make an app object for FTU

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/ftu/test_ftu_with_tour.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/ftu/test_ftu_with_tour.py
@@ -2,90 +2,46 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette.by import By
-
 from gaiatest import GaiaTestCase
+from gaiatest.apps.ftu.app import Ftu
 
 
 class TestFtu(GaiaTestCase):
 
-    _next_button_locator = (By.ID, 'forward')
-    _section_languages_locator = (By.ID, 'languages')
-    _take_tour_button_locator = (By.ID, 'lets-go-button')
-
-    # Section Tour
-    _step1_header_locator = (By.ID, 'step1Header')
-    _step2_header_locator = (By.ID, 'step2Header')
-    _step3_header_locator = (By.ID, 'step3Header')
-    _step4_header_locator = (By.ID, 'step4Header')
-    _step5_header_locator = (By.ID, 'step5Header')
-    _tour_next_button_locator = (By.ID, 'forward-tutorial')
-    _tour_back_button_locator = (By.ID, 'back-tutorial')
-
-    # Section Tutorial Finish
-    _section_tutorial_finish_locator = (By.CSS_SELECTOR, '.tutorial-finish-base')
-    _lets_go_button_locator = (By.ID, 'tutorialFinished')
-
     def setUp(self):
         GaiaTestCase.setUp(self)
-
-        # launch the First Time User app
-        self.app = self.apps.launch('FTU')
+        self.wait_for_condition(lambda m: self.data_layer.is_wifi_enabled)
 
     def test_ftu_with_tour(self):
+        ftu = Ftu(self.marionette)
+        ftu.launch()
+        ftu.wait_for_languages_display()
 
         # Go through the FTU setup as quickly as possible to get to the Tour section
-        self.wait_for_element_displayed(*self._section_languages_locator)
-
-        count = 0
-        while not self.is_element_displayed(*self._take_tour_button_locator):
-            if self.is_element_displayed(*self._next_button_locator):
-                self.marionette.find_element(*self._next_button_locator).tap()
-            else:
-                count=count+1
-            if count > 5:
-                break
+        ftu.run_ftu_setup()
 
         # Take the tour
-        self.marionette.find_element(*self._take_tour_button_locator).tap()
+        ftu.tap_take_tour()
 
         # Walk through the tour
-        self.wait_for_element_displayed(*self._step1_header_locator)
-        self.assertEqual(self.marionette.find_element(*self._step1_header_locator).text,
-                         "Swipe from right to left to browse your apps.")
-
-        # First time we see the next button we need to wait for it
-        self.wait_for_element_displayed(*self._tour_next_button_locator)
-        self.marionette.find_element(*self._tour_next_button_locator).tap()
-
-        self.wait_for_element_displayed(*self._step2_header_locator)
-        self.assertEqual(self.marionette.find_element(*self._step2_header_locator).text,
-                         "Tap and hold on an icon to delete or move it.")
-        self.marionette.find_element(*self._tour_next_button_locator).tap()
-
-        self.wait_for_element_displayed(*self._step3_header_locator)
-        self.assertEqual(self.marionette.find_element(*self._step3_header_locator).text,
-                         "Enter any keyword or topic and your phone will instantly adapt.")
-        self.marionette.find_element(*self._tour_next_button_locator).tap()
-
-        self.wait_for_element_displayed(*self._step4_header_locator)
-        self.assertEqual(self.marionette.find_element(*self._step4_header_locator).text,
-                         "Swipe down to access recent notifications, credit information and settings.")
-        self.marionette.find_element(*self._tour_next_button_locator).tap()
-
-        self.wait_for_element_displayed(*self._step5_header_locator)
-        self.assertEqual(self.marionette.find_element(*self._step5_header_locator).text,
-                         "Tap and hold the home button to browse and close recent apps.")
+        self.assertEqual(ftu.step1, "Swipe from right to left to browse your apps.")
+        ftu.tap_tour_next()
+        self.assertEqual(ftu.step2, "Tap and hold on an icon to delete or move it.")
+        ftu.tap_tour_next()
+        self.assertEqual(ftu.step3, "Enter any keyword or topic and your phone will instantly adapt.")
+        ftu.tap_tour_next()
+        self.assertEqual(ftu.step4, "Swipe down to access recent notifications, credit information and settings.")
+        ftu.tap_tour_next()
+        self.assertEqual(ftu.step5, "Tap and hold the home button to browse and close recent apps.")
 
         # Try going back a step
-        self.marionette.find_element(*self._tour_back_button_locator).tap()
-        self.wait_for_element_displayed(*self._step4_header_locator)
-        self.marionette.find_element(*self._tour_next_button_locator).tap()
-        self.wait_for_element_displayed(*self._step5_header_locator)
-        self.marionette.find_element(*self._tour_next_button_locator).tap()
-
-        self.wait_for_element_displayed(*self._section_tutorial_finish_locator)
-        self.marionette.find_element(*self._lets_go_button_locator).tap()
+        ftu.tap_back()
+        self.assertEqual(ftu.step4, "Swipe down to access recent notifications, credit information and settings.")
+        ftu.tap_tour_next()
+        self.assertEqual(ftu.step5, "Tap and hold the home button to browse and close recent apps.")
+        ftu.tap_tour_next()
+        ftu.wait_for_finish_tutorial_section()
+        ftu.tap_lets_go_button()
 
         # Switch back to top level now that FTU app is gone
         self.marionette.switch_to_frame()
