3e16dd3af0a88124ebb6339a5176ea46ca2adec0
Viorela Ioia <viorelaioia@gmail.com>
2014-05-22 18:15:15 +0300

    Bug 1008909 - Refactor test_inter_app_comm

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/system/test_inter_app_comm.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/system/test_inter_app_comm.py
@@ -2,31 +2,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import time
 from gaiatest import GaiaTestCase
-from marionette import Marionette
-from gaiatest.apps.iac_publisher.app import IacPublisher
+from gaiatest.apps.system.regions.iac_publisher import IacPublisher
 
 
 class TestInterAppComm(GaiaTestCase):
-
-    def setUp(self):
-        GaiaTestCase.setUp(self)
 
     def test_inter_app_comm(self):
         _testing_message = "this is a test"
 
         iac_publisher = IacPublisher(self.marionette)
         iac_publisher.launch()
-        iac_publisher.send_message(_testing_message)
+        iac_publisher.type_message(_testing_message)
+        iac_publisher.send_message()
+        iac_publisher.wait_for_message_received()
 
-        received_str_msg = self.marionette.find_element(*self._pub_app_received_str_msg_locator);
-        self.assertEqual(received_str_msg.get_attribute("value"), self._testing_message)
-
-        self.wait_for_element_present(*self._pub_app_received_blob_msg_locator)
-
-        received_blob_msg = self.marionette.find_element(*self._pub_app_received_blob_msg_locator);
-        self.assertEqual(received_blob_msg.get_attribute("value"), self._testing_message)
-
-        num_conns = self.marionette.find_element(*self._pub_app_num_conns_locator);
-        self.assertEqual(num_conns.get_attribute("value"), "1")
+        self.assertEqual(iac_publisher.received_str_message, _testing_message)
+        self.assertEqual(iac_publisher.received_blob_message, _testing_message)
+        self.assertEqual(iac_publisher.connections_number, "1")
